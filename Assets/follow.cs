e8b69cd8b45a67808584d3433c88e5a8836e9408
Alex Carter <alycarter@hotmail.co.uk>
2013-10-16 19:43:26 +0100

    initial commit
    initial commit
    ant model
    walk animation
    camera controller
    ant moves to clicked location

---
+++ Assets/follow.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class follow : MonoBehaviour {
+	
+	public Transform target;
+	public Quaternion angle;
+	private float distance = 6;
+	private float height = 3;
+	// Use this for initialization
+	void Start () {
+       angle =target.rotation;
+	}
+ 
+    void Update () {
+		if(Input.GetKey("left")){
+			angle = Quaternion.AngleAxis(angle.eulerAngles.y+(Time.deltaTime*90),Vector3.up);
+		}
+		if(Input.GetKey("right")){
+			angle = Quaternion.AngleAxis(angle.eulerAngles.y-(Time.deltaTime*90),Vector3.up);
+		}
+		if(Input.GetKey("down")){
+			height-=Time.deltaTime*4;
+		}
+		if(Input.GetKey("up")){
+			height+=Time.deltaTime*4;
+		}
+		if(Input.GetAxis("Mouse ScrollWheel")>0){
+			distance--;
+		}
+		if(Input.GetAxis("Mouse ScrollWheel")<0){
+			distance++;
+		}
+		Vector3 position = new Vector3(distance,target.localPosition.y+height,0);
+		position = angle * position;
+		position+=target.position;
+		RaycastHit hit;
+		if(Physics.Raycast(new Vector3(position.x,1000,position.z),Vector3.down,out hit)){
+			if(hit.point.y>position.y){
+				height+=hit.point.y-position.y;
+				position=hit.point;
+			}
+		}
+		position+=new Vector3(0,1,0);
+		transform.position=position;
+		transform.LookAt(target);
+    }
+ 
+}
+
+
---
+++ Assets/moveToLocation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveToLocation : MonoBehaviour {
+	
+	private bool mouseRelased = true;
+	private Vector3 targetLocation;
+	// Use this for initialization
+	void Start () {
+		targetLocation=transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			if(mouseRelased){
+				Ray ray=  Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				if(Physics.Raycast(ray, out hit)){
+					targetLocation=hit.point;
+				}
+				mouseRelased=false;
+			}
+		}else{
+			mouseRelased=true;
+		}
+		if(Vector3.Distance(targetLocation,transform.position)>0.5){
+			transform.LookAt(targetLocation);
+			transform.position= Vector3.MoveTowards(transform.position,targetLocation,Time.deltaTime*5);
+		}
+	}
+}
