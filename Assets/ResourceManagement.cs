fe7e0cbad811b07e088aab1c7f198af4750918ec
L9Aug <tbampton@hotmail.co.uk>
2013-10-20 22:47:36 +0100

    starting to make progress i think
    created a new c# script to work in, starting to develop a system for
    picking up and using resources will also soon start on displaying
    resources

---
+++ Assets/ResourceManagement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct Res { // this is a structure allowing us to have constant control over our resources.
+		public int Amount;
+	
+        public Res(int thing){
+        	Amount = 1000;
+		}
+}
+	
+
+		
+
+public class ResourceManagement : MonoBehaviour {
+	//public transform foodbrick;
+	// Use this for initialization
+	void Start () {
+		Res Food = new Res();
+		Res BuildingMaterial = new Res();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//transform.position = 2;
+		
+	}
+}
