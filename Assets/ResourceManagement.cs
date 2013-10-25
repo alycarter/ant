11b353a90855bfb94437ebf8d1131e45f67fafd6
L9Aug <tbampton@hotmail.co.uk>
2013-10-26 00:28:41 +0100

    got a functional food resource that counts down

--- Assets/ResourceManagement.cs
+++ Assets/ResourceManagement.cs
@@ -1,29 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public struct Res { // this is a structure allowing us to have constant control over our resources.
-		public int Amount;
-	
-        public Res(int thing){
-        	Amount = 1000;
-		}
-}
-	
-
-		
-
 public class ResourceManagement : MonoBehaviour {
-	//public transform foodbrick;
+	//variable declaration
+	float timedialation = 0;
+	public GUIText FValue;
+	
+	public struct Res { // this is a structure allowing us to have constant control over our resources.
+		public int Amount;	
+		
+        public Res(int thing){
+        	Amount = 1000;			
+		}
+	}
+	// creating the basic mats
+	Res Food = new Res();
+    
 	// Use this for initialization
 	void Start () {
-		Res Food = new Res();
-		Res BuildingMaterial = new Res();
-		
+		FValue.text = "1000";
+		Food.Amount = 1000;
+		//FValue.text = "Hello";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	//transform.position = 2;
 		
+		timedialation += Time.deltaTime;
+		if (timedialation >= 1)
+		{
+			timedialation = 0;
+			Food.Amount --;
+		}
+		//Food.Amount ++;
+		FValue.text = Food.Amount.ToString();
 	}
 }
