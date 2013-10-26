c127cdd7b957e130444366b5730af99788b74cc9
L9Aug <tbampton@hotmail.co.uk>
2013-10-26 02:29:48 +0100

    refined resources
    IM SO SORRY FOR THE PUN

--- Assets/ResourceManagement.cs
+++ Assets/ResourceManagement.cs
@@ -3,36 +3,51 @@ using System.Collections;
 
 public class ResourceManagement : MonoBehaviour {
 	//variable declaration
-	float timedialation = 0;
-	public GUIText FValue;
+	public GUIText FValue, MValue;
 	
-	public struct Res { // this is a structure allowing us to have constant control over our resources.
-		public int Amount;	
+	public struct Res { // this is a structure allowing us to have constant control over our resources.		
+		public int Amount;
+		public float DecayRate, gainRate, timer;
+		//public GUIText textO;
+		public void setValues(int x, float y, float z){
+        	Amount = x;
+			DecayRate = y;
+			gainRate = z;
+			timer = 0;
+		}      	
+	};			
 		
-        public Res(int thing){
-        	Amount = 1000;			
+	//funtion declaration
+	int resUpdate(ref Res x){
+		x.timer += Time.deltaTime;
+		if((x.DecayRate - x.gainRate) > 0){
+			if(x.timer >= (1/(x.DecayRate - x.gainRate))){
+				x.timer = 0;
+				x.Amount --;
+			}
+		}else if((x.DecayRate - x.gainRate) < 0){
+			if(x.timer >= (1/(x.gainRate - x.DecayRate))){
+				x.timer = 0;
+				x.Amount ++;
+			}
 		}
+		return x.Amount;
 	}
+	
 	// creating the basic mats
-	Res Food = new Res();
-    
+	public Res Food;
+    public Res Mats;
+	
 	// Use this for initialization
-	void Start () {
-		FValue.text = "1000";
-		Food.Amount = 1000;
-		//FValue.text = "Hello";
+	void Start () {		
+		Food.setValues(1000, 5, 3);
+		Mats.setValues(100, 0, 1);
+		print( 1 / Food.DecayRate);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		timedialation += Time.deltaTime;
-		if (timedialation >= 1)
-		{
-			timedialation = 0;
-			Food.Amount --;
-		}
-		//Food.Amount ++;
-		FValue.text = Food.Amount.ToString();
-	}
+		FValue.text = resUpdate(ref Food).ToString();
+		MValue.text = resUpdate(ref Mats).ToString();
+	}		
 }
